0e80d996d79b91bbf2b6a1c5977144caeb605b3f
Gnana Prakash <gnanaprakashkovuru007@gmail.com>
2025-12-29 22:40:55 +0530

    updated backend code

--- server.js
+++ server.js
@@ -1,35 +1,28 @@
-// server.js
 import dotenv from "dotenv";
-dotenv.config();
+ dotenv.config(); 
+ import express from "express"; 
+ import mongoose from "mongoose"; 
+ import cors from "cors"; 
+ import chatRoute from "./Routes/chatRoute.js"; 
+ import authRoute from "./Routes/authRoute.js"; 
+ import counsellingRoute from "./Routes/counsellingRoute.js"; 
+ const app = express(); 
+ const PORT = 5000;
 
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-
-import chatRoute from "./Routes/chatRoute.js";
-import authRoute from "./Routes/authRoute.js";
-import counsellingRoute from "./Routes/counsellingRoute.js";
-
-const app = express();
-
-
-app.use(cors({
-  origin: "*", 
-}));
-app.use(express.json());
-
-app.use("/api/auth", authRoute);
-app.use("/api/chat", chatRoute);
-app.use("/api/counselling", counsellingRoute);
-
-
-
-if (!mongoose.connection.readyState) {
-  mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB Connected"))
-    .catch((err) => console.error("MongoDB Error:", err));
-}
+ app.use(cors({ origin: "*", })); 
+ app.use(express.json()); 
+ app.use("/api/auth", authRoute); 
+ app.use("/api/chat", chatRoute); 
+ app.use("/api/counselling", counsellingRoute); 
+ if (!mongoose.connection.readyState) 
+  { mongoose .connect(process.env.MONGO_URI) .then(() => 
+    console.log("MongoDB Connected")) .
+    catch((err) => console.error("MongoDB Error:", err)); } 
+    
+  app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
 
 
-export default app;
+
+    export default app;
